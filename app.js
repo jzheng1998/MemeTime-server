1aa6745435855a1f53b8067b2f5b71ced60c7e20
Jack Zheng <jackzheng784@gmail.com>
2020-12-10 02:30:43 -0500

    Initialize backend for dynamic web final

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "dynamic-web-b49a8.firebaseapp.com",
+  projectId: "dynamic-web-b49a8",
+  storageBucket: "dynamic-web-b49a8.appspot.com",
+  messagingSenderId: "872225899077",
+  appId: "1:872225899077:web:db64176ff0c96930b7992c",
+};
+
+const firebase = require("firebase");
+// Initialize Firebase
+if (firebase.app.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Routes Import
+const indexRoute = require("./routes/index.js");
+
+// Routes
+app.use("/", indexRoute);
+
+app.listen(port, () => {
+  console.log("Jack's Dynamic Web Application Final is running!");
+});
